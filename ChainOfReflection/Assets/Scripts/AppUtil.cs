40ba803dafaa63988e5ff2f9899d46a42cd65951
Miho Ijuin <miiiiiiiii747@gmail.com>
2019-03-21 18:40:21 +0900

    [add]refs #3 一つ目の光を実装

--- ChainOfReflection/Assets/Scripts/AppUtil.cs
+++ ChainOfReflection/Assets/Scripts/AppUtil.cs
@@ -10,17 +10,40 @@ public class AppUtil : MonoBehaviour
         DOTween.useSmoothDeltaTime = true;
     }
 
-    public static void DOSequence(Tween[] tweenArray, float prependInterval, float appendInterval){
+    public static Sequence DOSequence(Tween[] tweenArray, float prependInterval, float appendInterval, int loopTime=1){
         Sequence sequence = DOTween.Sequence();
         sequence.PrependInterval(prependInterval);
         sequence.AppendInterval(appendInterval);
         foreach(Tween tween in tweenArray){
             sequence.Append(tween);
         }
+        sequence.SetLoops(loopTime);
+        return sequence;
     }
 
-    public static IEnumerator WaitForTween(Tween tween){
+    public static IEnumerator WaitDO(Tween tween){
         yield return tween.WaitForCompletion();
+    }
+
+    public static IEnumerator WaitDO(Sequence sequence){
+        yield return sequence.WaitForCompletion();
+    }
+
+
+    public static void KillDO(Tween tween, bool complete=true){
+        tween.Kill(complete);
+    }
+
+    public static void KillDO(Sequence sequence, bool complete=true){
+        sequence.Kill(complete);
+    }
+
+    public static void SetOnKillCallback(Tween tween, TweenCallback action){
+        tween.OnKill(action);
+    }
+
+    public static void SetOnKillCallback(Sequence sequence, TweenCallback action){
+        sequence.OnKill(action);
     }
 
     public static Tween ShowRect(RectTransform rect, string changeValue, float startValue, float duration, string ease, float delay=0){
@@ -66,6 +89,12 @@ public class AppUtil : MonoBehaviour
         rect.anchoredPosition = startAnchoredPos;
         Vector2  targetAnchoredPos = endAnchoredPos;
         Tween tween = rect.DOAnchorPos(targetAnchoredPos, duration).SetEase(easeType).SetDelay(delay);
+        return tween;
+    }
+
+    public static Tween Rotate(Transform target, Vector3 endValue, float duration, string ease="OutQuad", float delay=0f){
+        Ease easeType = (Ease)Enum.Parse(typeof(Ease), ease);
+        Tween tween = target.DORotate(endValue, duration).SetEase(easeType).SetDelay(delay);
         return tween;
     }
 }
